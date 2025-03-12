fd0e8899e556781f1f9cf87d3f2605d28bf25264
aria <aria.afsar@gmail.com>
2025-03-12 14:57:37 +0330

    added the start manu

---
+++ python/app/main.py
@@ -0,0 +1,21 @@
+from .best_dns import Best_dns
+from .dns_list_checker import Dns_list
+from .os_dns_checker import Os_dns
+
+class menu :
+    def start_menu():
+        print("Hello World!")
+        print("#################################################")
+        while True :
+            print("choose you option please: \n 1.check system dns \n 2.set dns from list \n 3.check fastest dns")
+            print("#################################################")
+            choice = input()
+            print("#################################################")
+            if choice == 1 or choice == "1" :
+                pass
+            elif choice == 2 or choice == "2" :
+                pass
+            elif choice == 3 or choice == "3" :
+                pass
+            else:
+                print("bad request")
