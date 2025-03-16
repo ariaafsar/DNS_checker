0b7589252ba737015703dc800c7f2f190716cc72
aria <aria.afsar@gmail.com>
2025-03-16 12:12:07 +0330

    cleand up the menu a little bit

--- python/app/main.py
+++ python/app/main.py
@@ -3,15 +3,16 @@ from os_dns_checker import Os_dns
 class menu :
     def start_menu():
         print("Hello World!")
-        print("#################################################")
         while True :
+            print("#################################################")
             print("choose you option please: \n 1.check system dns \n 2.set dns from list \n 3.check fastest dns \n 4.exit")
             print("#################################################")
             choice = input()
             print("#################################################")
             if choice == 1 or choice == "1" :
+                print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
                 Os_dns.check_os()
-                break
+                print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
             elif choice == 2 or choice == "2" :
                 pass
             elif choice == 3 or choice == "3" :
