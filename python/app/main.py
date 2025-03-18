cc7a46fa42dba9bc56ce557f0a8367e82c34c532
aria <aria.afsar@gmail.com>
2025-03-18 12:38:00 +0330

    added dns seter input

--- python/app/main.py
+++ python/app/main.py
@@ -1,11 +1,12 @@
 from os_dns_checker import Os_dns
-
+from dns_seter import Seter
+import sys
 class menu :
     def start_menu():
         print("Hello World!")
         while True :
             print("#################################################")
-            print("choose you option please: \n 1.check system dns \n 2.set dns from list \n 3.check fastest dns \n 4.exit")
+            print("choose you option please: \n 1.check system dns \n 2.set dns \n 3.check fastest dns \n 4.exit")
             print("#################################################")
             choice = input()
             print("#################################################")
@@ -21,7 +22,13 @@ class menu :
                     choice1 = input()
                     print("#################################################")
                     if choice1 == "1" or choice1 == 1 :
-                        pass
+                        print("enter your dns ip")
+                        dns_ip = input()
+                        if not Seter.dns_validator(dns_ip) :
+                            print("invalid dns ip")
+                            sys.exit(1)
+                        else :
+                            Seter.set_dns(dns_ip)
                     elif choice1 == "2" or choice == 2 :
                         pass
                     elif choice1 == "3" or choice == 3 :
